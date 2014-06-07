9b608e37c4661a0709af76f58847efe1d4c9f800
Martin Rue <hello@martinrue.com>
2014-06-07 19:04:07 +0100

    add events for registration being turned on/off and enable throttle

--- routes/admin.js
+++ routes/admin.js
@@ -2,6 +2,13 @@ var db = require('../lib/db');
 var events = require('../lib/events');
 var basicAuth = require('./middleware/basic-auth');
 
+var raiseEvent = function(message) {
+  events.add({
+    name: 'MancJS Bot',
+    gravatar: db.getGravatarUrl('mancjs@martinrue.com')
+  }, message);
+};
+
 var routes = function(app) {
   app.get('/admin', basicAuth, function(req, res) {
     return res.json(db.getTeams());
@@ -13,10 +20,12 @@ var routes = function(app) {
   });
 
   app.get('/admin/registration/enable', basicAuth, function(req, res) {
+    raiseEvent('Registration is now open :)');
     return res.json(db.enableRegistration());
   });
 
   app.get('/admin/registration/disable', basicAuth, function(req, res) {
+    raiseEvent('Registration is now closed');
     return res.json(db.disableRegistration());
   })
 };
